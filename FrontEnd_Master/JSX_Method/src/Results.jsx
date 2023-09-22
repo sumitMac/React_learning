2ea4e8f7bcf2cefa4ca9635ab928419baa6c8e89
sumitMac <sumit.kumar@Technoidentity.com>
2023-09-22 10:34:28 +0530

    22 sep

--- FrontEnd_Master/JSX_Method/src/Results.jsx
+++ FrontEnd_Master/JSX_Method/src/Results.jsx
@@ -4,17 +4,18 @@ const Results = ({ pets }) => {
   return (
     <div className="search">
       {!pets.length ? (
-        <h1>No Pets are found</h1>
+        <h1>No Pets Found 😕🙁</h1>
       ) : (
         pets.map((pet) => {
           return (
             <Pet
               animal={pet.animal}
-              name={pet.name}
-              bread={pet.bread}
-              image={pet.image}
-              location={`${location}`}
               key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
             />
           );
         })
