e5b820bbfa0fd086be5356539bd903c0ecc15ed2
sumitMac <sumit.kumar@Technoidentity.com>
2023-09-27 14:54:49 +0530

    27 sep

--- FrontEnd_Master/JSX_Method/src/Details.jsx
+++ FrontEnd_Master/JSX_Method/src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 
 const Details = () => {
@@ -31,4 +32,10 @@ const Details = () => {
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
